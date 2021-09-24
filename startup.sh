ec50b2c5e5ba4d9ffff01be94f53882a3a0a7a2e
tusqasi <2002.tushar.kuntawar@gmail.com>
2021-09-24 14:24:31 +0530

    wallpaper

--- startup.sh
+++ startup.sh
@@ -4,16 +4,17 @@
 
 
 pactl set-sink-mute 0 1 &
-firefox &
+# firefox &
 xmodmap ~/.xmodmap 
 xcape -e 'Control_R=Escape' -t 400 &
-picom &
+# picom &
 copyq &
 nm-applet &
 volctl &
-~/scripts/battery_notify.py &
-feh Pictures/Wallpapers/light_mode/950834.png --bg-max &
+# ~/scripts/battery_notify.py &
+feh ~/.config/variety/Downloaded/Bing/OHR.ShyGuy_EN-US7880739914_1920x1080.jpg --bg-max
 lxpolkit &
 xfce4-power-manager &
-unclutter --timeout 3 &
+# unclutter --timeout 3 &
+tmux new -d -s sys
 alacritty -e tmux &
